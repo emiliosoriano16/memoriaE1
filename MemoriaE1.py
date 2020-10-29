d4a74a989140d1fbdee3a8951a42c44d08e3bcb4
unknown <A01658895@itesm.mx>
2020-10-29 12:38:05 -0600

    Se alineo correctamente los números y se puso el marcador de los taps

--- MemoriaE1.py
+++ MemoriaE1.py
@@ -6,6 +6,7 @@ car = path('car.gif')
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
+numTaps = 0
 
 def square(x, y):
     "Draw white square with black outline at (x, y)."
@@ -38,7 +39,10 @@ def tap(x, y):
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
+    global numTaps 
+    numTaps=numTaps+1
 
+                
 def draw():
     "Draw image and tiles."
     clear()
@@ -56,16 +60,19 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 4, y)
+        goto(x + 25, y)
         color('red')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        write(tiles[mark], font=('Arial', 30, 'normal'), align="center")
+    goto (210,150)
+    write(numTaps)
+
 
     update()
     ontimer(draw, 100)
 
 
 shuffle(tiles)
-setup(420, 420, 370, 0)
+setup(500, 420, 370, 0)
 addshape(car)
 hideturtle()
 tracer(False)
